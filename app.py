425c3c1798febabc99811c9759751ea79ea037fe
mbrsagor <mbrsagor@gmail.com>
2021-04-01 16:37:35 +0600

    Basic route concept and logic of falsk router

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -10,6 +10,30 @@ def index():
 def about():
     return "I'm Sagor from Dhaka, Bangladesh"
 
+@app.route('/hello/<name>')
+def sayHello(name):
+    return f"Hello {name}!"
 
+@app.route('/task/<int:taskId>')
+def taskList(taskId):
+    return f"Task id {taskId}"
+
+@app.route('/admin')
+def hello_admin():
+   return 'Hello Admin'
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+   return f"Hello {guest}"
+
+@app.route('/user/<name>')
+def hello_user(name):
+   if name =='admin':
+      return redirect(url_for('hello_admin'))
+   else:
+      return redirect(url_for('hello_guest',guest = name))
+
+
+# run the project on local dev server
 if __name__ == '__main__':
     app.run()
