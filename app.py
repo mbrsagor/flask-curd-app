7ec702e6adb835d0cb651fe1774a0009a17cc7d6
mbrsagor <mbrsagor@gmail.com>
2021-04-01 19:30:02 +0600

    Remove database command line

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+from datetime import datetime
+
 from flask import Flask, redirect, render_template, url_for
+from flask_sqlalchemy import sqlalchemy
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db'
+# db = sqlalchemy(app)
+
+# class Todo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     content = db.Column(db.String(150))
+#     completed = db.Column(db.Integer, default=0)
+#     date = db.Column(db.DateTime, default=datetime.utcnow)
+
+#     def __repr__(self):
+#         return f"Task {self.id}"
+
 
 @app.route('/')
 def index():
