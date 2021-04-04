522504bab362c4fb76996d52164f6bce3f4abf9d
mbrsagor <mbrsagor@gmail.com>
2021-04-04 07:19:06 +0600

    Database connection issues fixed

--- models.py
+++ models.py
@@ -4,7 +4,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Todo models
